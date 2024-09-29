964597dd130f2139baa70e8f7894109bae7af7fb
Justin Arista Kasin <30282499+zegheim@users.noreply.github.com>
2024-09-29 18:47:13 +0100

    enable multi-account, multi-day bookings

--- terraform/eventbridge_scheduler.tf
+++ terraform/eventbridge_scheduler.tf
@@ -11,31 +11,11 @@ data "aws_iam_policy_document" "eventbridge_scheduler_assume_role_policy_documen
 }
 
 resource "aws_iam_role" "eventbridge_scheduler_execution_role" {
-  name               = "${var.lambda_name}-SchedulerExecutionRole"
+  name               = "${local.lambda_name}-SchedulerExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.eventbridge_scheduler_assume_role_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "allow_eventbridge_scheduler_to_execute_lambda" {
   role       = aws_iam_role.eventbridge_scheduler_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
-}
-
-resource "aws_scheduler_schedule" "execute_lambda_on_schedule" {
-  name = "${var.lambda_name}-SchedulerExecution"
-
-  flexible_time_window {
-    mode = "OFF"
-  }
-
-  schedule_expression          = var.eventbridge_scheduler_schedule_expression
-  schedule_expression_timezone = "Europe/London"
-
-  target {
-    arn      = aws_lambda_function.book_better_bot.arn
-    role_arn = aws_iam_role.eventbridge_scheduler_execution_role.arn
-
-    retry_policy {
-      maximum_retry_attempts = 0
-    }
-  }
 }
--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "lambda_name" {
-  description = "Name of the generated Lambda function"
-  value       = aws_lambda_function.book_better_bot.function_name
-}

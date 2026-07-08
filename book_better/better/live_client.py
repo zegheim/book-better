b3a5001e5cd752e952eed492dd13f68f28331226
Justin Arista Kasin <hello@aristakas.in>
2026-07-08 08:26:19 +0700

    update to use v2 api

--- book_better/better/live_client.py
+++ book_better/better/live_client.py
@@ -99,15 +99,31 @@ class LiveBetterClient:
         start_time: datetime.time,
         end_time: datetime.time,
     ) -> list[ActivitySlot]:
-        response = self.session.get(
-            f"activities/venue/{venue.value}/activity/{activity.value}/slots",
+        times = self.session.get(
+            f"activities/venue/{venue.value}/activity/{activity.value}/v2/times",
+            params={"date": activity_date.strftime("%Y-%m-%d")},
+        )
+        times.raise_for_status()
+
+        composite_key = next(
+            time["composite_key"]
+            for time in times.json()["data"]
+            if time["starts_at"]["format_24_hour"] == start_time.strftime("%H:%M")
+            and time["ends_at"]["format_24_hour"] == end_time.strftime("%H:%M")
+            and time["spaces"] > 0
+            and time["booking"] is None
+        )
+
+        slots = self.session.get(
+            f"activities/venue/{venue.value}/activity/{activity.value}/v2/slots",
             params={
                 "date": activity_date.strftime("%Y-%m-%d"),
                 "start_time": start_time.strftime("%H:%M"),
                 "end_time": end_time.strftime("%H:%M"),
+                "composite_key": composite_key,
             },
         )
-        response.raise_for_status()
+        slots.raise_for_status()
 
         return [
             ActivitySlot(
@@ -118,7 +134,7 @@ class LiveBetterClient:
                 name=slot["location"]["slug"],
                 cart_type=slot["cart_type"],
             )
-            for slot in response.json()["data"]
+            for slot in slots.json()["data"]
             if slot["spaces"] > 0
             and slot["booking"] is None
             and slot["benefit_available"] is not None
